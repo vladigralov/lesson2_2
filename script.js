c0410cd52cdc80236086dfac82e0b99a5622d506
vladigralov <53748671+vladigralov@users.noreply.github.com>
2019-08-05 13:07:50 +0300

    correct Math

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 console.log("begin");
 
 //математические функции
-Math.pow(2,-3);
-Math.sqrt(9);
-Math.ceil(1.25);//up
-Math.round(1.25);
-Math.sign(42);
-Math.max(3,5,6,2);
-Math.min(5,-9,0,1);
+console.log(Math.pow(2,-3));
+console.log(Math.sqrt(9));
+console.log(Math.ceil(1.25));//up
+console.log(Math.round(1.25));
+console.log(Math.sign(42));
+console.log(Math.max(3,5,6,2));
+console.log(Math.min(5,-9,0,1));
