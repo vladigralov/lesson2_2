79d4cc60e619196a06b1afbf3e35e5fea00b8ead
vladigralov <53748671+vladigralov@users.noreply.github.com>
2019-08-05 13:04:23 +0300

    Math

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
 console.log("begin");
 
-//строки
-console.log("\" \n Hi")
-let string = " The quick brown fox quickly jumps over the lazy dog. "
-console.log(string[0] + string[5]);
-console.log(string.indexOf("fox"));
-console.log(string.indexOf("o"));
-console.log(string.lastIndexOf("o"));
-console.log(string.replace("quick","slow"));
-console.log(string.split(" "));
-let splited = (string.split(" "));
-console.log(splited);
-console.log(string.split("o"));
-console.log(splited.join("-"));
-console.log(string.substr(16,3));
-console.log(string.toLowerCase());
-console.log(string.toUpperCase());
-console.log(string);
-console.log(string.trim());
+//математические функции
+Math.pow(2,-3);
+Math.sqrt(9);
+Math.ceil(1.25);//up
+Math.round(1.25);
+Math.sign(42);
+Math.max(3,5,6,2);
+Math.min(5,-9,0,1);
